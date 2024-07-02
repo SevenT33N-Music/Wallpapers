2ac1f34ef5c515e4936fa68a77bf3d14a5492688
Tyler Kelson <135186375+SevenT33N-Music@users.noreply.github.com>
2024-07-02 04:10:01 +0000

    more stuff
    Updated styles, more functionality, added more images

--- script.js
+++ script.js
@@ -20,17 +20,46 @@ function setFilter(filter) {
   }
 }
 function checkFilter(search) {
-  let wallpaperList = document.querySelectorAll('.wallpaper-item');
-  for (i = 0; i < wallpaperList.length; i++) {
-    let item = wallpaperList[i];
-    let lowerSearch = search.toLowerCase();
-    let classes = item.classList;
-    let className = classes[1];
-    if (className.includes(lowerSearch)) {
-      item.style.display = 'block';
+  if (search.length > 0) {
+    let results = 0;
+    let hr = document.querySelectorAll('hr');
+    let h1 = document.querySelectorAll('h1');
+    for (i = 0; i < hr.length; i++) {
+      let itemHR = hr[i];
+      let itemH1 = h1[i];
+      itemHR.style.display = "none";
+      itemH1.style.display = "none";
     }
-    else {
-      item.style.display = 'none';
+    let wallpaperList = document.querySelectorAll('.wallpaper-item');
+    for (i = 0; i < wallpaperList.length; i++) {
+      let item = wallpaperList[i];
+      let lowerSearch = search.toLowerCase();
+      let classes = item.classList;
+      let className = classes[1];
+      if (className.includes(lowerSearch)) {
+        item.style.display = 'block';
+        results += 1;
+      }
+      else {
+        item.style.display = 'none';
+      }
+    }
+    document.getElementById('searchNumber').innerHTML = `Search Results: ${results}`;
+  }
+  else {
+    document.getElementById('searchNumber').innerHTML = "";
+    let hr = document.querySelectorAll('hr');
+    let h1 = document.querySelectorAll('h1');
+    for (i = 0; i < hr.length; i++) {
+      let itemHR = hr[i];
+      let itemH1 = h1[i];
+      itemHR.style.display = "block";
+      itemH1.style.display = "block";
+    }
+    let wallpaperList = document.querySelectorAll('.wallpaper-item');
+    for (i = 0; i < wallpaperList.length; i++) {
+      let item = wallpaperList[i];
+      item.style.display = 'block';
     }
   }
 }
